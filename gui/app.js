e7a02802350047a632aa0865677a519de562766d
Jerry Zhao <xyzjerry@gmail.com>
2016-08-01 20:26:49 -0700

    add mobile friendly table style
    Change-Id: I5b97c8850fc8944df4071eb2c03b44f875114074

--- gui/app.js
+++ gui/app.js
@@ -1,31 +1,48 @@
 (function() {
   var app = angular.module('piguard', []);
   app.controller('AgentsController', ['$scope', '$http', function($scope, $http) {
+    var api_url = 'http://127.0.0.1:8000'
     var agents = this;
+    var config = {
+      headers : {
+          'Content-Type': 'application/json'
+      }
+    }
     agents.alarms = {}
     $scope.arm = function(deviceid, on) {
       var json_data = {
         alarm_on: on
       }
-      var config = {
-        headers : {
-            'Content-Type': 'application/json'
-        }
-      }
-      $http.put('http://127.0.0.1:8000/agents/' + deviceid, json_data, config).success(function() {
+      $http.put( api_url + '/agents/' + deviceid, json_data, config).success(function() {
         $scope.getAgents()
       });
     }
     $scope.getAgents = function() {
-      $http.get('http://127.0.0.1:8000/agents').success(function(agents_data) {
+      $http.get( api_url + '/agents').success(function(agents_data) {
         agents.items = agents_data;
       });
     }
     $scope.getAlarms = function(deviceid) {
-      $http.get('http://127.0.0.1:8000/alarms?deviceid=' + deviceid).success(function(alarms_data) {
+      $http.get( api_url + '/alarms?deviceid=' + deviceid).success(function(alarms_data) {
         agents.alarms[deviceid] = alarms_data
     });
     }
+
+    $scope.ack_alarm = function(deviceid, alarmid) {
+      var alarm_data = {
+        'state': 'acknowledged'
+      }
+      $http.put( api_url + '/alarms/' + alarmid, alarm_data, config).success(function() {
+        $scope.getAlarms(deviceid)
+      });
+    }
+
+    $scope.del_alarm = function(deviceid, alarmid) {
+      $http.delete( api_url + '/alarms/' + alarmid, config).success(function() {
+        $scope.getAlarms(deviceid)
+      });
+    }
+
     $scope.getAgents()
   }]);
 
